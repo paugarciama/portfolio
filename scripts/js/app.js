5a6ad30568eb94d8505170d8c14d4e94949f243a
Pau Garcia Martínez <etpau@hotmail.es>
2021-04-07 19:11:28 +0200

    Finish responsive menu

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -1,15 +1,44 @@
-// NAV BAR MOBILE EVENT
+// SHOW BURGER MENU
 var mobileMenu = document.querySelector(".nav-menu-mobile");
+var modalMenu = document.querySelector(".menu-modal");
+var icon1 = document.querySelector('.icon1');
+var icon2 = document.querySelector('.icon2');
 var burger = document.querySelector(".mobile-btn");
 burger.addEventListener('click', function (e) {
     if (mobileMenu.classList.contains('inactive')) {
         mobileMenu.classList.remove('inactive');
+        modalMenu.classList.remove('inactive');
     }
     else {
         mobileMenu.classList.add('inactive');
+        modalMenu.classList.add('inactive');
     }
-    var icon1 = document.querySelector('.icon1');
-    var icon2 = document.querySelector('.icon2');
+    changeIcons();
+});
+// HIDE BURGER MENU 
+var links = document.querySelectorAll('.nav-link-mobile');
+links.forEach(function (target) { return target.addEventListener('click', function (e) {
+    if (!mobileMenu.classList.contains('inactive')) {
+        changeDisplay();
+    }
+}); });
+var resumeBtn = document.querySelector('.resume-btn');
+resumeBtn.addEventListener('click', function (e) {
+    if (!mobileMenu.classList.contains('inactive')) {
+        changeDisplay();
+    }
+});
+modalMenu.addEventListener('click', function (e) {
+    if (!mobileMenu.classList.contains('inactive')) {
+        changeDisplay();
+    }
+});
+function changeDisplay() {
+    mobileMenu.classList.add('inactive');
+    modalMenu.classList.add('inactive');
+    changeIcons();
+}
+function changeIcons() {
     if (icon2.classList.contains('inactive')) {
         icon2.classList.remove('inactive');
         icon1.classList.add('inactive');
@@ -18,17 +47,4 @@ burger.addEventListener('click', function (e) {
         icon1.classList.remove('inactive');
         icon2.classList.add('inactive');
     }
-});
-// HIDE MENU 
-var links = document.querySelectorAll('.nav-link-mobile');
-links.forEach(function (target) { return target.addEventListener('click', function (e) {
-    if (!mobileMenu.classList.contains('inactive')) {
-        mobileMenu.classList.add('inactive');
-    }
-}); });
-var resumeBtn = document.querySelector('.resume-btn');
-resumeBtn.addEventListener('click', function (e) {
-    if (!mobileMenu.classList.contains('inactive')) {
-        mobileMenu.classList.add('inactive');
-    }
-});
+}
--- scripts/ts/app.ts
+++ scripts/ts/app.ts
@@ -1,18 +1,49 @@
-// NAV BAR MOBILE EVENT
+// SHOW BURGER MENU
 const mobileMenu = document.querySelector(".nav-menu-mobile") as HTMLDivElement;
+const modalMenu = document.querySelector(".menu-modal") as HTMLDivElement;
+const icon1 = document.querySelector('.icon1') as HTMLSpanElement;
+const icon2 = document.querySelector('.icon2') as HTMLSpanElement;
 
 const burger = document.querySelector(".mobile-btn") as HTMLButtonElement;
 burger.addEventListener('click', e => {
   if (mobileMenu.classList.contains('inactive')) {
     mobileMenu.classList.remove('inactive');
+    modalMenu.classList.remove('inactive');
   } else {
     mobileMenu.classList.add('inactive');
+    modalMenu.classList.add('inactive');
   }
+  changeIcons();
+})
 
-  const icon1 = document.querySelector('.icon1') as HTMLSpanElement;
-  const icon2 = document.querySelector('.icon2') as HTMLSpanElement;
+// HIDE BURGER MENU 
+const links = document.querySelectorAll('.nav-link-mobile') as NodeList;
+links.forEach(target => target.addEventListener('click', e => {
+  if (!mobileMenu.classList.contains('inactive')) {
+    changeDisplay();
+  }
+}))
 
+const resumeBtn = document.querySelector('.resume-btn') as HTMLButtonElement;
+resumeBtn.addEventListener('click', e => {
+  if (!mobileMenu.classList.contains('inactive')) {
+    changeDisplay();
+  }
+})
 
+modalMenu.addEventListener('click', e => {
+  if (!mobileMenu.classList.contains('inactive')) {
+    changeDisplay();
+  }
+})
+
+function changeDisplay(): void {
+  mobileMenu.classList.add('inactive');
+  modalMenu.classList.add('inactive');
+  changeIcons();
+}
+
+function changeIcons(): void {
   if (icon2.classList.contains('inactive')) {
     icon2.classList.remove('inactive');
     icon1.classList.add('inactive');
@@ -20,19 +51,4 @@ burger.addEventListener('click', e => {
     icon1.classList.remove('inactive');
     icon2.classList.add('inactive');
   }
-})
-
-// HIDE MENU 
-const links = document.querySelectorAll('.nav-link-mobile') as NodeList;
-links.forEach(target => target.addEventListener('click', e => {
-  if (!mobileMenu.classList.contains('inactive')) {
-    mobileMenu.classList.add('inactive');
-  }
-}))
-
-const resumeBtn = document.querySelector('.resume-btn') as HTMLButtonElement;
-resumeBtn.addEventListener('click', e => {
-  if (!mobileMenu.classList.contains('inactive')) {
-    mobileMenu.classList.add('inactive');
-  }
-})
+}
